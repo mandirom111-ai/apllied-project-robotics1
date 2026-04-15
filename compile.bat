aab16a3626a5544278e032249d0fe12e7b9c1278
rajan bashyal <mandirom111@gmail.com>
2026-04-15 11:47:03 +0300

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,2 +1,7 @@
 "C:\Program Files\Java\jdk1.7.0_80\bin\javac" -classpath "lib\ev3classes.jar" src\helloworld.java
 "C:\Program Files\Java\jdk1.7.0_80\bin\javac" -classpath "lib\ev3classes.jar" src\moter.java
+"C:\Program Files\Java\jdk1.7.0_80\bin\javac" -classpath "lib\ev3classes.jar" src\LightSensorReader.java
+"C:\Program Files\Java\jdk1.7.0_80\bin\javac" -classpath "lib\ev3classes.jar" src\UltrasonicReader.java
+
+
+
--- package.bat
+++ package.bat
@@ -1,2 +1,5 @@
-"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe helloworld.jar src.helloworld -C. src
-"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe moter.jar src.moter -C. src
+
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe helloworld.jar src.helloworld -C src .
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe moter.jar src.moter -C src .
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe Light.jar src.LightMain -C src .
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe Ultrasonic.jar src.UltrasonicMain -C src .
